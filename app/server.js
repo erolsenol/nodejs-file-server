406811adeff867d53ef4ef2dad29a1456b4535c8
erolsenol <erol.senol@outlook.com>
2024-04-13 17:23:08 +0300

    change

--- app/server.js
+++ app/server.js
@@ -18,38 +18,44 @@ app.get('/ping', function (req, res) {
   res.send('pong');
 });
 
-app.post('/upload', async function (req, res) {
-  let file;
-  let uploadPath;
+app.post('/upload', function (req, res) {
+  try {
+    let file;
+    let uploadPath;
 
-  if (!req.files || Object.keys(req.files).length === 0) {
-    res.status(400).send('No files were uploaded.');
-    return;
-  }
-
-  console.log('req.files >>>', req.files); // eslint-disable-line
-
-  file = req.files.file;
-
-  uploadPath = path.resolve(__dirname, WRITE_PATH + file.name);
-
-  console.log('uploadPath', uploadPath);
-  const fileStat = fs.existsSync(uploadPath);
-  console.log('fileStat', fileStat);
-  if (fileStat) {
-    return res.status(400).json({
-      success: false,
-      error: 'file_already_exists',
-    });
-  }
-
-  file.mv(uploadPath, function (err) {
-    if (err) {
-      return res.status(500).send(err);
+    if (!req.files || Object.keys(req.files).length === 0) {
+      res.status(400).send('No files were uploaded.');
+      return;
     }
 
-    res.send('File uploaded to ' + uploadPath);
-  });
+    file = req.files.file;
+    uploadPath = path.resolve(__dirname, WRITE_PATH + file.name);
+
+    const fileCheck = fs.existsSync(uploadPath);
+    if (fileCheck) {
+      return res.status(400).json({
+        success: false,
+        error: 'file_already_exists',
+      });
+    }
+
+    file.mv(uploadPath, function (err) {
+      if (err) {
+        return res.status(500).send(err);
+      }
+
+      res.json({
+        success: true,
+        path: uploadPath,
+        message: 'file_uploaded',
+      });
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      error,
+    });
+  }
 });
 
 app.listen(PORT, function () {
